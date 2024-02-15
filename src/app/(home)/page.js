0d8f842debee38f9e4cd9ed89eec8367130d9519
rishabh <hespnod2348@gmai.com>
2024-02-15 14:45:26 +0530

    made the layout for first header component

---
+++ src/app/(home)/page.js
@@ -0,0 +1,9 @@
+import Header1 from "@/components/Header1";
+
+export default function Home() {
+  return (
+    <div className=''>
+      <Header1 />
+    </div>
+  );
+}
---
+++ src/components/Header1.jsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import Block from './Block'
+
+const Header1 = () => {
+    return (
+        <div className='flex h-24 px-10 justify-between border-b-2 items-center'>
+            <Image src="/logo.png" alt="oyo_logo" width={200} height={200} className='h-28 w-28' />
+            <div className=' flex h-full'>
+                <Block title='Become a member' para='Additional 10% off on stays' />
+                <Block title='OYO for business' para='Trusted by 5000 corporates' />
+                <Block title='List your property' para='Start earning in 30 min.' />
+                <Block title='9205097674' para='Call me to hire for giving your website a new look.' />
+                <div className="flex items-center px-3">
+                    <Image src="/demo.svg" alt="oyo_logo" width={200} height={200} className='h-10 w-10 rounded-full mr-5' />
+                    <h3 className='font-bold'>Login/Signup</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header1
