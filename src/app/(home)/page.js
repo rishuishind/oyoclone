a1acf437c782f82f1872853a91a608b8c524c269
rishabh <hespnod2348@gmai.com>
2024-02-15 17:05:00 +0530

    changed the metadata

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -1,6 +1,8 @@
 import Header1 from "@/components/Header1";
 import Header2 from "@/components/Header2";
 import Header3 from "@/components/Header3";
+import Image from 'next/image'
+import Head from 'next/head'
 
 export default function Home() {
   return (
@@ -8,6 +10,14 @@ export default function Home() {
       <Header1 />
       <Header2 />
       <Header3 />
+      <div className="mx-20">
+        <div className="my-14">
+          <Image src="/banner1.avif" alt="banner1" width={200} height={200} className=" w-full" />
+        </div>
+        <div className="mb-14">
+          <Image src="/banner2.avif" alt="banner2" width={200} height={200} className=" w-full" />
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Oyo Clone",
+  title: "OYO: India's Best Online Hotel Booking Site For Sanatized Stay.",
   description: "Website for Oyo hotels",
 };
 
